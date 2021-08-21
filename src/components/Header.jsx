4021232bb1041a21854bd544712556f59814edd5
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-08-22 02:52:23 +0300

    do home pages maket

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Free_logo.svg" alt="logo"/>
+    </header>
+)
+}
+
+export default Header
