9be3d71f1dc217dc4b0032af9efab5fe4d4bf1ce
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-08-22 04:25:11 +0300

    split into folders

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import s from './MyPosts.module.css'
+import Post from "../Post/Post";
+
+
+const MyPosts = () => {
+    return (
+        <div>
+            My posts
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+            <div className={s.posts}>
+                <div>
+                    New posts
+                </div>
+            </div>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+        </div>
+    )
+}
+
+export default MyPosts
